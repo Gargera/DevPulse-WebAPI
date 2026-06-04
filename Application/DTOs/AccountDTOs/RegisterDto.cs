53bc2e3f21e524aaddab14d9041133b9406a7622
Gargera <et93512@gmail.com>
2026-06-04 06:53:21 +0300

    Add account DTOs with validation

---
+++ Application/DTOs/AccountDTOs/RegisterDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.AccountDTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string FirstName { get; set; } = string.Empty;
+        
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string LastName { get; set; } = string.Empty;
+        
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9]+$")]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(8)]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
